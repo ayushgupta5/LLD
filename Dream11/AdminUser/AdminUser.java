8f9c8d0b1952ede6958564009583b1bd4e5d6217
Ayush Gupta <ayushgupta.ac@gmail.com>
2024-09-21 09:37:45 +0530

    feat:[Ayush Dream 11] Added the changes based on MR Comments

--- Dream11/AdminUser/AdminUser.java
+++ Dream11/AdminUser/AdminUser.java
@@ -7,10 +7,12 @@ public class AdminUser extends User implements  IAdminUser {
         super(userID, userName, email, walletBalance);
     }
 
+    @Override
     public void createContest(Game game, double entryFee, double prizePool) {
         System.out.println("Allows the admin to create a contest for a specific game.");
     }
 
+    @Override
     public void manageUser() {
         System.out.println("Admin can manage user accounts (not detailed here).");
     }
--- Dream11/AdminUser/IAdminUser.java
+++ Dream11/AdminUser/IAdminUser.java
@@ -1,4 +1,7 @@
 package AdminUser;
 
+import Game.Game;
 public interface IAdminUser {
+    void createContest(Game game, double entryFee, double prizePool);
+    void manageUser();
 }
